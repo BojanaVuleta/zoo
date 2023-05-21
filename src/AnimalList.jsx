6958b784929bd473064fd71bdde6a160d26a29df
BojanaVuleta <vuleta.bojana8@gmail.com>
2023-05-21 18:39:11 +0200

    prvi zadatak

---
+++ src/AnimalList.jsx
@@ -0,0 +1,42 @@
+const listOfAnimals = [
+    {
+      name: "krava",
+      species: "mamel",
+      date_of_birth: new Date().toDateString(),
+    },
+    {
+      name: "delfin",
+      species: "mamel",
+    },
+    {
+      name: "koza",
+      species: "mamel",
+    },
+    {
+      name: "ovca",
+      species: "mamel",
+      date_of_birth: new Date().toDateString(),
+    },
+    {
+      name: "pas",
+      species: "mamel",
+      date_of_birth: new Date().toDateString(),
+    },
+  ];
+
+  const AnimalList=()=>{
+    return (
+      
+        <div>
+          {listOfAnimals.map((animal, index) => (
+            <div key={index}>
+              {animal.name}
+              {animal.species}
+              {animal.date_of_birth}
+            </div>
+          ))}
+        </div>
+      )
+  }
+
+  export default AnimalList;
